8eada03f19a79a6920f698c275f0d00cd6d96571
김교휘 <hwee000@naver.com>
2024-02-19 16:30:24 +0900

    init

---
+++ huewith/src/main/java/com/example/huewith/HuewithApplication.java
@@ -0,0 +1,13 @@
+package com.example.huewith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuewithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuewithApplication.class, args);
+	}
+
+}
---
+++ huewith/src/test/java/com/example/huewith/HuewithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.huewith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuewithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
